598583f8e653333948c5458284f85ed9ee89af57
Retta <moges.retta@wur.nl>
2021-03-19 14:46:20 +0100

    Estimation Rd and plotting of Rd for replicates done

--- Estimate_FvCB_parameters.py
+++ Estimate_FvCB_parameters.py
@@ -16,29 +16,67 @@ class Estimate_FvCB_parameters:
         self.gas_exch_measurement=gas_exch_measurement
         
 
-    def plot_predictions(self,slope,intercept,X,y):
+    def plot_Rd(self,Rds,points):
+            fig, ax = plt.subplots(2,2,constrained_layout=True)
+            replicates = Rds['Replicate'].values
+            count = 0
+            for replicate in replicates:
+                Rd = Rds['Rd'].values
+                slope = Rds['Slope'].values
+
+                X = points['X'][count]
+                Y = points['Y'][count]
+                R2 = Rds['R2'].values
+                ypredict = slope[count]*X+Rd[count]
                 
-            ypredict = slope*X+intercept
-            plt.scatter(X, y,  color='black')
-            plt.plot(X, ypredict, color='blue', linewidth=3)            
-            plt.xticks(())
-            plt.yticks(())  
-            plt.xlabel('\u03A6$_{PSII}$ * $I_{inc}$/4 (µmol $m^{-2}$ $s^{-1}$)')
-            plt.ylabel('Net photosynthesis (µmol $m^{-2}$ $s^{-1}$)')
+                if Rd[count]>0:
+                    rd_text = ' + ' + str(np.round(Rd[count],2))                    
+                else:
+                    rd_text = ' - ' + str(np.round(abs(Rd[count]),2))
+                text = 'y = ' + str(np.round(slope[count],3)) + '*x '+\
+                rd_text+' \n R2 = '+str(np.round(R2[count],3))
+                
+                if replicate == 1:
+                    ax[0][0].plot(X, Y, 'ko')           
+                    ax[0][0].plot(X, ypredict, color='black', linewidth=3) 
+                    ax[0][0].set_ylabel('Net photosynthesis (µmol $m^{-2}$ $s^{-1}$)')
+                    ax[0][0].text(min(X)+1,max(Y)-1,text)
+                    
+                elif replicate==2:
+                    ax[0][1].plot(X, Y, 'ko')           
+                    ax[0][1].plot(X, ypredict, color='black', linewidth=3)
+                    ax[0][1].text(min(X)+1,max(Y)-1,text)
+                     
+                elif replicate==3:
+                    ax[1][0].plot(X, Y, 'ko')         
+                    ax[1][0].plot(X, ypredict, color='black', linewidth=3)
+                    ax[1][0].set_xlabel('\u03A6$_{PSII}$ * $I_{inc}$/4 (µmol $m^{-2}$ $s^{-1}$)') 
+                    ax[1][0].set_ylabel('Net photosynthesis (µmol $m^{-2}$ $s^{-1}$)')
+                    ax[1][0].text(min(X)+1,max(Y)-1,text)
+                    
+                else:
+                    ax[1][1].plot(X, Y, 'ko')           
+                    ax[1][1].plot(X, ypredict, color='black', linewidth=3) 
+                    ax[1][1].set_xlabel('\u03A6$_{PSII}$ * $I_{inc}$/4 (µmol $m^{-2}$ $s^{-1}$)')
+                    ax[1][1].text(min(X)+1,max(Y)-1,text)
+                count+=1           
             plt.show()
             
 # Estimate Rd and calibration factor s
     def estimate_Rd(self): 
         
         """
-        Estimate Rd as intercept of linear regression between A and PhiPSII*Iinc/4
+        Estimate Rd as intercept of linear regression between 
+        A and PhiPSII*Iinc/4
         """
         
         AI = self.gas_exch_measurement.get_AI_data()
         AI=AI[AI['Irradiance']<230]
         replicates = AI['Replicate'].values
         replicates=np.unique(replicates)
-        df = pd.DataFrame([],columns=['Rd','Std.err','R2'] ) 
+        df = pd.DataFrame([],columns=['Replicate','Rd','Std.err','R2','Slope']) 
+        df2 = pd.DataFrame([],columns=['Replicate','X','Y'] ) 
+        
         count = 0
         for replicate in replicates:
             A_I_r= AI[AI['Replicate']==replicate]
@@ -50,8 +88,14 @@ class Estimate_FvCB_parameters:
             df.loc[count,'Rd'] = intercept
             df.loc[count,'Std.err'] = std_err
             df.loc[count,'R2'] = r_value**2
-            self.plot_predictions(slope,intercept,X,A)
+            df.loc[count,'Replicate'] = replicate
+            df.loc[count,'Slope'] = slope
+            
+            df2.loc[count,'X'] = X
+            df2.loc[count,'Y'] = A
+            df2.loc[count,'Replicate'] = replicate
             count+=1
+        self.plot_Rd(df,df2)
             
             
             
