leakage correction formula corrected for to account CO2s correction

Analyze_csv_files.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-FORMAT = ['Photo','Cond','Ci',"Fv/Fm", 'PhiPS2','CO2S','PARi','Trmmol','BLCond']
+FORMAT = ['Photo','Cond','Ci',"CO2R",'CO2S','PhiPS2','CO2S','PARi','Trmmol','BLCond']
 PATH = (r'\\WURNET.NL\Homes\retta001\My Documents\Project\2021\GasExchange\\')
 species_code = ['Hi','Bn']
 treatment =['HL','LL']
@@ -51,17 +51,18 @@ def plot_response(treatment,data,measurement_days):
                 A = ACI['Photo'].values
                 gs = ACI['Cond'].values
                 PhiPS2 = ACI['PhiPS2'].values.astype(float)
-                FvFm = ACI["Fv/Fm"].values.astype(float)
-                ax[0][0].plot(np.sort(Ci,axis=None), np.sort(A,axis=None),symbol,fillstyle='none',markersize=8)
-                ax[0][1].plot(np.sort(Ci,axis=None), np.sort(gs,axis=None),symbol,fillstyle='none',markersize=8)
-                ax[1][0].plot(np.sort(Ci,axis=None), np.sort(PhiPS2,axis=None),symbol,fillstyle='none',markersize=8)
-                ax[1][1].plot(np.sort(Ci,axis=None), np.sort(FvFm,axis=None),symbol,label='Day'+str(measurement_days[i]),fillstyle='none',markersize=8)
+                CO2R = ACI["CO2R"].values.astype(float)
+#                CO2S = ACI["CO2S"].values.astype(float)                
+                ax[0][0].plot(Ci, A,symbol,fillstyle='none',markersize=8)
+                ax[0][1].plot(Ci, gs,symbol,fillstyle='none',markersize=8)
+                ax[1][0].plot(Ci, PhiPS2,symbol,fillstyle='none',markersize=8)
+                ax[1][1].plot(Ci, CO2R,symbol,label='Day'+str(measurement_days[i]),fillstyle='none',markersize=8)
                 ax[0][0].set_ylabel("Net photosynthesis (µmol $m^{-2}$ $s^{-1}$)")
                 ax[0][1].set_ylabel("Stomatal conductance (mol $m^{-2}$ $s^{-1}$)")
                 ax[1][0].set_ylabel("\u03A6$_{PSII}$ (-)")
-                ax[1][1].set_ylabel("Fv/Fm (-)")
+                ax[1][1].set_ylabel("Ci (µmol $mol^{-1}$)")
                 ax[1][0].set_xlabel("Intercellular $CO_2$ (µmol $mol^{-1}$)")
-                ax[1][1].set_xlabel("Intercellular $CO_2$ (µmol $mol^{-1}$)")
+                ax[1][1].set_xlabel("External $CO_2$ (µmol $mol^{-1}$)")
                 ax[1][1].set_ylim(bottom=0.7)
                 ax[1][1].legend(loc='lower right', fontsize='x-large')     
                 
@@ -74,15 +75,15 @@ def plot_response(treatment,data,measurement_days):
             A = AI['Photo'].values
             gs = AI['Cond'].values
             PhiPS2 = AI['PhiPS2'].values.astype(float)
-            FvFm = AI["Fv/Fm"].values.astype(float)
-            ax[0][0].plot(np.sort(I,axis=None), np.sort(A,axis=None),symbol,fillstyle='none',markersize=8)
-            ax[0][1].plot(np.sort(I,axis=None), np.sort(gs,axis=None),symbol,fillstyle='none',markersize=8)
-            ax[1][0].plot(np.sort(I,axis=None), np.sort(PhiPS2,axis=None),symbol,fillstyle='none',markersize=8)
-            ax[1][1].plot(np.sort(I,axis=None), np.sort(FvFm,axis=None),symbol,label='Day'+str(measurement_days[i]),fillstyle='none',markersize=8)
+            Ci = AI["Ci"].values
+            ax[0][0].plot(I, A,symbol,fillstyle='none',markersize=8)
+            ax[0][1].plot(I, gs,symbol,fillstyle='none',markersize=8)
+            ax[1][0].plot(I, PhiPS2,symbol,fillstyle='none',markersize=8)
+            ax[1][1].plot(I, Ci,symbol,label='Day'+str(measurement_days[i]),fillstyle='none',markersize=8)
             ax[0][0].set_ylabel("Net photosynthesis (µmol $m^{-2}$ $s^{-1}$)")
             ax[0][1].set_ylabel("Stomatal conductance (mol $m^{-2}$ $s^{-1}$)")
             ax[1][0].set_ylabel("\u03A6$_{PSII}$ (-)")
-            ax[1][1].set_ylabel("Fv/Fm (-)")
+            ax[1][1].set_ylabel("Ci (µmol $mol^{-1}$)")
             ax[1][0].set_xlabel("Irradiance (µmol $m^{-2}$ $s^{-1}$)")
             ax[1][1].set_xlabel("Irradiance (µmol $m^{-2}$ $s^{-1}$)")
             ax[1][1].set_ylim(bottom=0.7)
@@ -94,7 +95,7 @@ def plot_response(treatment,data,measurement_days):
 #FORMAT = ['Photo','Cond','Ci','Fv/Fm', 'PhiPS2','CO2S','PARi']
 
 def replicates_to_Excel(data_frame,species,Oxygen,curve,treatment):
-    columns = ['Replicate','Species','Treatment','Measurement type','Oxygen level','Net CO2 assimilation rate','Intercellular CO2 concentration','PhiPS2','Irradiance','Stomatal conductance for CO2','CO2S','Trmmol','BLCond']
+    columns = ['Replicate','Species','Treatment','Measurement type','Oxygen level','Net CO2 assimilation rate','Intercellular CO2 concentration','PhiPS2','Irradiance','Stomatal conductance for CO2','CO2S','CO2R','Trmmol','BLCond']
     Gas_Exchange_data = pd.DataFrame([],columns=columns )
     for i in range(0,len(data_frame)):
         AI = data_frame[i]
@@ -103,6 +104,8 @@ def replicates_to_Excel(data_frame,species,Oxygen,curve,treatment):
         gs = AI['Cond'].values
         Ci = AI['Ci'].values
         CO2S = AI['CO2S'].values
+        CO2R = AI['CO2R'].values
+        
         Trmmol = AI['Trmmol'].values
         BLCond = AI['BLCond'].values
         
@@ -119,13 +122,14 @@ def replicates_to_Excel(data_frame,species,Oxygen,curve,treatment):
         df1['Irradiance'] = I[:]
         df1['Stomatal conductance for CO2'] = gs[:]
         df1['CO2S'] = CO2S[:]
+        df1['CO2R'] = CO2R[:]        
         df1['Trmmol'] = Trmmol[:]
         df1['BLCond'] = BLCond[:]
         
         Gas_Exchange_data=Gas_Exchange_data.append(df1)
     return Gas_Exchange_data
   
-columns = ['Replicate','Species','Treatment','Measurement type','Oxygen level','Net CO2 assimilation rate','Intercellular CO2 concentration','PhiPS2','Irradiance','Stomatal conductance for CO2','CO2S','Trmmol','BLCond']
+columns = ['Replicate','Species','Treatment','Measurement type','Oxygen level','Net CO2 assimilation rate','Intercellular CO2 concentration','PhiPS2','Irradiance','Stomatal conductance for CO2','CO2S','CO2R','Trmmol','BLCond']
 Gas_Exchange_data = pd.DataFrame([],columns=columns )   
 
 # B.Nigra LL

Main.py
@@ -263,14 +263,196 @@ ax[1].set_xlabel("Intercellular $CO_2$ (µmol $mol^{-1}$)",fontsize=16)
 ax[1].legend(loc='lower right')  
 plt.show()
 
-# Estimate Jmax
+# Estimate FvCB kinetics and Jmax
+Jmax_values = pd.DataFrame([], columns=['Species','Treatment','Replicate','Jmax','theta','Jmax_err','theta_err'])
+
+def Jmax_tabel(Jmaxs,species,treatment):
+        df = pd.DataFrame([], columns=['Species','Treatment','Replicate','Jmax','theta','Jmax_err','theta_err'])
+        df['Species']=[species]*4
+        df['Treatment']=[treatment]*4
+        df['Jmax']=Jmaxs['Jmax'].values
+        df['Replicate']=Jmaxs['Replicate'].values
+        df['Jmax_err']=Jmaxs['Jmax_err'].values
+        df['theta']=Jmaxs['theta'].values
+        df['theta_err']=Jmaxs['theta_err'].values
+        
+        return df
+
+
+Phi2LL_values = pd.DataFrame([], columns=['Species','Treatment','Replicate','Phi2LL','Phi2LL_err'])
+
+def Phi2LL_tabel(Phi2LLs,species,treatment):
+        df = pd.DataFrame([], columns=['Species','Treatment','Replicate','Phi2LL','Phi2LL_err'])
+        df['Species']=[species]*4
+        df['Treatment']=[treatment]*4
+        df['Phi2LL']=Phi2LLs['Phi2LL'].values
+        df['Replicate']=Phi2LLs['Replicate'].values
+        df['Phi2LL_err']=Phi2LLs['Phi2LL_err'].values
+        
+        return df
+    
+    
+cols = ['Plant','Treatment','Vcmax','Rd','Sco','Tp','Jmax','k2LL','theta',\
+        'Vcmax_err','Rd_err','Sco_err','Tp_err','Jmax_err','theta_err']
+df_params=pd.DataFrame([],columns=cols)
 
 species = 'B.Nigra'
 treatment = 'LL'
-O = 0.21
-s = 0.524
+O = 0.02
 gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
 parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+Rd_Bn_LL = parameters.estimate_Rd()
+Rd = np.mean(Rd_Bn_LL['Rd'].values,axis=0)
+s = np.nanmean(Rd_Bn_LL['Slope'].values,axis=0)
+Rd_err = np.nanstd(Rd_Bn_LL['Rd'].values/4,axis=0)
+
+O = 0.21
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+phi2LL_individual = parameters.estimate_individual_phi2LL()
 phi2LL = parameters.estimate_phi2LL()
-inputs = {'s':s,'phi2LL':phi2LL[0][0]}
-Jmax = parameters.estimate_Jmax(inputs)
+phi2LLs=Phi2LL_tabel(phi2LL_individual,species,treatment)
+Phi2LL_values=Phi2LL_values.append(phi2LLs)
+k2_Bn_LL = parameters.calculate_k2(Rd_Bn_LL['Slope'])
+
+inputs = {'s':Rd_Bn_LL['Slope'].values,'phi2LL':phi2LL_individual['Phi2LL'].values}
+Jmax_individual  = parameters.estimate_individual_Jmax(inputs)
+inputs = {'s':s,'PHI2LL':phi2LL[0][0]}
+Jmax  = parameters.estimate_Jmax(inputs)
+Jmaxs=Jmax_tabel(Jmax_individual,species,treatment)
+Jmax_values=Jmax_values.append(Jmaxs)
+
+df_params.loc[0,'Rd']=Rd
+df_params.loc[0,'Rd_err']=Rd_err
+df_params.loc[0,'k2LL']=s*phi2LL[0][0]
+df_params.loc[0,'Jmax']=Jmax[0][0]
+df_params.loc[0,'Jmax_err']=Jmax[1]
+df_params.loc[0,'theta']=Jmax[0][1]
+df_params.loc[0,'theta_err']=Jmax[2]
+df_params.loc[0,'Plant']=species
+df_params.loc[0,'Treatment']=treatment
+
+species = 'B.Nigra'
+treatment = 'HL'
+O = 0.02
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+Rd_Bn_LL = parameters.estimate_Rd()
+Rd = np.mean(Rd_Bn_LL['Rd'].values,axis=0)
+s = np.nanmean(Rd_Bn_LL['Slope'].values,axis=0)
+Rd_err = np.nanstd(Rd_Bn_LL['Rd'].values/4,axis=0)
+
+O = 0.21
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+phi2LL_individual = parameters.estimate_individual_phi2LL()
+phi2LL = parameters.estimate_phi2LL()
+phi2LLs=Phi2LL_tabel(phi2LL_individual,species,treatment)
+Phi2LL_values=Phi2LL_values.append(phi2LLs)
+k2_Bn_HL = parameters.calculate_k2(Rd_Bn_LL['Slope'])
+
+parameters.compare_k2(k2_Bn_HL,k2_Bn_LL,'HL','LL')
+
+inputs = {'s':Rd_Bn_LL['Slope'].values,'phi2LL':phi2LL_individual['Phi2LL'].values}
+Jmax_individual  = parameters.estimate_individual_Jmax(inputs)
+inputs = {'s':s,'PHI2LL':phi2LL[0][0]}
+Jmax  = parameters.estimate_Jmax(inputs)
+Jmaxs=Jmax_tabel(Jmax_individual,species,treatment)
+Jmax_values=Jmax_values.append(Jmaxs)
+
+df_params.loc[1,'Rd']=Rd
+df_params.loc[1,'Rd_err']=Rd_err
+df_params.loc[1,'k2LL']=s*phi2LL[0][0]
+df_params.loc[1,'Jmax']=Jmax[0][0]
+df_params.loc[1,'Jmax_err']=Jmax[1]
+df_params.loc[1,'theta']=Jmax[0][1]
+df_params.loc[1,'theta_err']=Jmax[2]
+df_params.loc[1,'Plant']=species
+df_params.loc[1,'Treatment']=treatment
+
+species = 'H.Incana'
+treatment = 'LL'
+O = 0.02
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+Rd_Bn_LL = parameters.estimate_Rd()
+Rd = np.mean(Rd_Bn_LL['Rd'].values,axis=0)
+s = np.nanmean(Rd_Bn_LL['Slope'].values,axis=0)
+Rd_err = np.nanstd(Rd_Bn_LL['Rd'].values/4,axis=0)
+
+O = 0.21
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+phi2LL_individual = parameters.estimate_individual_phi2LL()
+phi2LL = parameters.estimate_phi2LL()
+phi2LLs=Phi2LL_tabel(phi2LL_individual,species,treatment)
+Phi2LL_values=Phi2LL_values.append(phi2LLs)
+k2_Hi_LL = parameters.calculate_k2(Rd_Bn_LL['Slope'])
+
+inputs = {'s':Rd_Bn_LL['Slope'].values,'phi2LL':phi2LL_individual['Phi2LL'].values}
+Jmax_individual  = parameters.estimate_individual_Jmax(inputs)
+inputs = {'s':s,'PHI2LL':phi2LL[0][0]}
+Jmax  = parameters.estimate_Jmax(inputs)
+Jmaxs=Jmax_tabel(Jmax_individual,species,treatment)
+Jmax_values=Jmax_values.append(Jmaxs)
+
+df_params.loc[2,'Rd']=Rd
+df_params.loc[2,'Rd_err']=Rd_err
+df_params.loc[2,'k2LL']=s*phi2LL[0][0]
+df_params.loc[2,'Jmax']=Jmax[0][0]
+df_params.loc[2,'Jmax_err']=Jmax[1]
+df_params.loc[2,'theta']=Jmax[0][1]
+df_params.loc[2,'theta_err']=Jmax[2]
+df_params.loc[2,'Plant']=species
+df_params.loc[2,'Treatment']=treatment
+
+species = 'H.Incana'
+treatment = 'HL'
+O = 0.02
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+Rd_Bn_LL = parameters.estimate_Rd()
+Rd = np.mean(Rd_Bn_LL['Rd'].values,axis=0)
+s = np.nanmean(Rd_Bn_LL['Slope'].values,axis=0)
+Rd_err = np.nanstd(Rd_Bn_LL['Rd'].values/4,axis=0)
+
+O = 0.21
+gas_exch_measurement = Gas_exchange_measurement(O,species,treatment)
+parameters = Estimate_FvCB_parameters(gas_exch_measurement)
+phi2LL_individual = parameters.estimate_individual_phi2LL()
+phi2LL = parameters.estimate_phi2LL()
+phi2LLs=Phi2LL_tabel(phi2LL_individual,species,treatment)
+Phi2LL_values=Phi2LL_values.append(phi2LLs)
+k2_Hi_HL = parameters.calculate_k2(Rd_Bn_LL['Slope'])
+
+parameters.compare_k2(k2_Hi_HL,k2_Hi_LL,'HL','LL')
+
+
+inputs = {'s':Rd_Bn_LL['Slope'].values,'phi2LL':phi2LL_individual['Phi2LL'].values}
+Jmax_individual  = parameters.estimate_individual_Jmax(inputs)
+inputs = {'s':s,'PHI2LL':phi2LL[0][0]}
+Jmax  = parameters.estimate_Jmax(inputs)
+Jmaxs = Jmax_tabel(Jmax_individual,species,treatment)
+Jmax_values = Jmax_values.append(Jmaxs)
+
+df_params.loc[3,'Rd']=Rd
+df_params.loc[3,'Rd_err']=Rd_err
+df_params.loc[3,'k2LL']=s*phi2LL[0][0]
+df_params.loc[3,'Jmax']=Jmax[0][0]
+df_params.loc[3,'Jmax_err']=Jmax[1]
+df_params.loc[3,'theta']=Jmax[0][1]
+df_params.loc[3,'theta_err']=Jmax[2]
+df_params.loc[3,'Plant']=species
+df_params.loc[3,'Treatment']=treatment
+
+#Jmax_values.to_excel(PATH +'Parameters_Jmax_theta.xlsx', index = False)
+boxplot = Jmax_values.boxplot(column=['Jmax'], by=['Treatment','Species'],figsize = (8,10),grid=False,layout=(2, 1),fontsize=16)
+boxplot[0].set_ylabel('J$_{max}$ (µmol $m^{-2}$ $s^{-1}$)',fontsize=16)
+
+#Phi2LL_values.to_excel(PATH +'Parameters_Phi2LL.xlsx', index = False)
+boxplot = Phi2LL_values.boxplot(column=['Phi2LL'], by=['Treatment','Species'],figsize = (8,10),grid=False,layout=(2, 1),fontsize=16)
+boxplot[0].set_ylabel("\u03A6$_{PSII,LL}$ (-)",fontsize=16)
+
+parameters.compare_k2(k2_Hi_HL,k2_Bn_HL,'H.Incana','B.Nigra')
+parameters.compare_k2(k2_Hi_LL,k2_Bn_LL,'H.Incana','B.Nigra')
+